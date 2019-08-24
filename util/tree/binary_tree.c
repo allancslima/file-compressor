b00f89cdf9521448d5b3839d02887a60ad4778a1
Allan Lima <allan.lima@ilhasoft.com.br>
2019-08-24 19:04:11 -0300

    Add Binary Tree ADT

---
+++ util/tree/binary_tree.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_tree.h"
+
+binary_tree_t* binary_tree_create(void *data, binary_tree_t *left, binary_tree_t *right)
+{
+    binary_tree_t *binary_tree = (binary_tree_t*) malloc(sizeof(binary_tree_t));
+    binary_tree->data = data;
+    binary_tree->left = left;
+    binary_tree->right = right;
+
+    return binary_tree;
+}
+
+void binary_tree_in_order(binary_tree_t *binary_tree, void (*block)(void *data))
+{
+    if (binary_tree != NULL) {
+        binary_tree_in_order(binary_tree->left, block);
+        block(binary_tree->data);
+        binary_tree_in_order(binary_tree->right, block);
+    }
+}
+
+void binary_tree_pre_order(binary_tree_t *binary_tree, void (*block)(void *data))
+{
+    if (binary_tree != NULL) {
+        block(binary_tree->data);
+        binary_tree_pre_order(binary_tree->left, block);
+        binary_tree_pre_order(binary_tree->right, block);
+    }
+}
+
+void binary_tree_post_order(binary_tree_t *binary_tree, void (*block)(void *data))
+{
+    if (binary_tree != NULL) {
+        binary_tree_post_order(binary_tree->left, block);
+        binary_tree_post_order(binary_tree->right, block);
+        block(binary_tree->data);
+    }
+}
---
+++ util/tree/binary_tree.h
@@ -0,0 +1,52 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+/**
+ * Binary tree alias.
+ */
+typedef struct _binary_tree binary_tree_t;
+
+/**
+ * Binary tree struct.
+ */
+struct _binary_tree {
+    void *data;
+    binary_tree_t *left;
+    binary_tree_t *right;
+};
+
+/**
+ * Allocates memory to a node containing pointers to the data, the left node and the right node.
+ *
+ * @param data pointer to the allocated data.
+ * @param left left binary tree.
+ * @param right right binary tree.
+ * @return pointer to {@link binary_tree_t}
+ */
+binary_tree_t* binary_tree_create(void *data, binary_tree_t *left, binary_tree_t *right);
+
+/**
+ * Traverses binary tree from a root in normal order.
+ *
+ * @param binary_tree pointer to a binary tree root.
+ * @param block pointer to function that receives the data of the node tree.
+ */
+void binary_tree_in_order(binary_tree_t *binary_tree, void (*block)(void *data));
+
+/**
+ * Traverses binary tree from a root in pre order.
+ *
+ * @param binary_tree pointer to a binary tree root.
+ * @param block pointer to function that receives the data of the node tree.
+ */
+void binary_tree_pre_order(binary_tree_t *binary_tree, void (*block)(void *data));
+
+/**
+ * Traverses binary tree from a root in post order.
+ *
+ * @param binary_tree pointer to a binary tree root.
+ * @param block pointer to function that receives the data of the node tree.
+ */
+void binary_tree_post_order(binary_tree_t *binary_tree, void (*block)(void *data));
+
+#endif
